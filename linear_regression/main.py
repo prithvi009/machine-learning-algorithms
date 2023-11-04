1e08d489509b387c1d15e7e2f6e9cd024550183b
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2023-11-05 02:25:53 +0530

    linear_regression

---
+++ linear_regression/main.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+from LinearRegression import LinearRegression
+
+def mean_squared_error(y_true, y_pred):
+    return np.mean((y_true - y_pred)**2)
+
+X, y = datasets.make_regression(n_samples=100, n_features=1, noise=20, random_state=4)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+regressor = LinearRegression(lr=0.001)
+fig = plt.figure(figsize=(8,6))
+plt.scatter(X[:, 0], y, color = "b", marker = "o", s = 30)
+plt.show()
+
+regressor.fit(X_train, y_train)
+predictions = regressor.predict(X_test)
+
+mse = mean_squared_error(y_test, predictions)
+print("MSE:", mse)
+
